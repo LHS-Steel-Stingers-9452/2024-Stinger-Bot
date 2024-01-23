0cc11bd91d8e2221262ca9a132c5bf3b69a1343b
Kevin <kevinjaramillo247@gmail.com>
2024-01-22 19:21:36 -0800

    file complete

--- Stinger-Bot/src/main/java/frc/robot/SwerveModuleConstants.java
+++ Stinger-Bot/src/main/java/frc/robot/SwerveModuleConstants.java
@@ -1,5 +1,17 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 public class SwerveModuleConstants {
+    public final int driveMotorCanID;
+    public final int angleMotorCanID;
+    public final int canCoderID;
+    public final Rotation2d canCoderOffset;
+
+    public SwerveModuleConstants(int driveMotorCanID, int angleMotorCanID, int canCoderID, Rotation2d canCoderOffset) {
+        this.driveMotorCanID = driveMotorCanID;
+        this.angleMotorCanID = angleMotorCanID;
+        this.canCoderID = canCoderID;
+        this.canCoderOffset = canCoderOffset;
+    }
     
 }
