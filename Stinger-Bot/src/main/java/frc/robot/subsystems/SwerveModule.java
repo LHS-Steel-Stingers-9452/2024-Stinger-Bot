f200e8d11fbd601d11951dcb3657ca9ca4c8e9ba
Kevin <Kevinjaramillo247@gmail.com>
2024-02-07 11:50:18 -0800

    removed unnecessary imports and files

--- Stinger-Bot/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ Stinger-Bot/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -22,7 +22,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.PWM.PeriodMultiplier;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ctre.phoenix6.hardware.CANcoder;
