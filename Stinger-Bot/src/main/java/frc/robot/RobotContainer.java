6d9bb97e6a52c9c4bd98728c99dc6023266115bc
Kevin Jaramillo <kevinjaramillo247@gmail.com>
2024-02-20 09:53:50 -0800

    Robot container updated

--- Stinger-Bot/src/main/java/frc/robot/RobotContainer.java
+++ Stinger-Bot/src/main/java/frc/robot/RobotContainer.java
@@ -9,13 +9,15 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+//import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
 import frc.robot.Constants.ControllerConstants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.LauncherConstants;
 import frc.robot.Constants.TransferConstants;
+import frc.robot.commands.IntakeNoteReg;
 import frc.robot.commands.TeleopSwerve;
 import frc.robot.subsystems.drive.SwerveBase;
-
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.launcher.Shooter;
 import frc.robot.subsystems.transfer.Transfer;
@@ -26,22 +28,25 @@ public class RobotContainer {
   private final CommandXboxController operatorController = new CommandXboxController(ControllerConstants.operatorControllerPort);
 
   private final SwerveBase swerveBase;
-  private final TeleopSwerve teleopSwerve;
-
   private final Intake intakeSub;
-
   private final Transfer transferSub;
-
   private final Shooter launcherSub;
+
+  private TeleopSwerve teleopSwerve;
+  private IntakeNoteReg intakeNoteReg;
+
+
 
   public RobotContainer() {
 
     //initialize subsytems
     swerveBase = new SwerveBase();
 
+    intakeSub = new Intake();
+
     transferSub = new Transfer();
 
-    intakeSub = new Intake();
+    launcherSub = new Shooter();
 
     teleopSwerve = new TeleopSwerve(
       swerveBase,
@@ -53,10 +58,6 @@ public class RobotContainer {
       );
     
     swerveBase.setDefaultCommand(teleopSwerve);
-
-    //test shooter kraken motors 02/17/2024
-    launcherSub = new Shooter();
-    
 
     configureBindings();
   }
@@ -72,15 +73,34 @@ public class RobotContainer {
     driverController.y().onTrue(new InstantCommand(() -> swerveBase.zeroGyro()));
 
     
-    //operator controller bindings
-    operatorController.rightBumper().onTrue(new InstantCommand(() -> transferSub.setTransferSpeed(TransferConstants.transferSeed)));
-    operatorController.leftBumper().onTrue(new InstantCommand(() -> transferSub.setTransferSpeed(TransferConstants.tranSpitSpeed)));
-    transferSub.setDefaultCommand(new InstantCommand(() -> transferSub.stopTransfer()));
+    //operator controller bindings for manual control and testing 
+    /*add vision to determine speed and angle necessary to score in speaker */
+    /*Arm, shooter, and transfer should eventually be in the same command for shooting */
+    operatorController.rightTrigger().whileTrue(new InstantCommand(() -> launcherSub.setShooterSpeed(LauncherConstants.lowPositionSpeed,  LauncherConstants.ffOvercomeGrav)));
+    operatorController.leftTrigger().whileTrue(new InstantCommand(() -> launcherSub.setShooterSpeed(-LauncherConstants.intakeFromShooterSpeed, LauncherConstants.ffOvercomeGrav)));
+    operatorController.y().onTrue(new InstantCommand(() -> launcherSub.stopShooter()));
 
-    
-    
+    operatorController.rightBumper().whileTrue(
+      new IntakeNoteReg(
+        intakeSub, 
+        IntakeConstants.intakeSpeed, 
+        transferSub, 
+        TransferConstants.transferSeed));
 
-    
+      operatorController.leftBumper().whileTrue(
+        new IntakeNoteReg(
+          intakeSub, 
+          IntakeConstants.intakeSpitSpeed, 
+          transferSub, 
+          TransferConstants.tranSpitSpeed));
+
+
+
+    //intakeSub.setDefaultCommand(new InstantCommand(() -> intakeSub.stopIntake()));
+    //transferSub.setDefaultCommand(new InstantCommand(() -> transferSub.stopTransfer()));
+
+    //Arm
+  
   
   }
 
