711a3ca4879ba6d0d1fede192ab46bb4f64542e5
Kevin <Kevinjaramillo247@gmail.com>
2024-03-31 14:39:53 -0700

    Integrating KG term to arm

--- Stinger-Bot/src/main/java/frc/robot/Constants.java
+++ Stinger-Bot/src/main/java/frc/robot/Constants.java
@@ -151,7 +151,8 @@ public class Constants {
         public static final int leadID = 19;
         public static final int followID = 20;
         public static final double kS = 0;//.91
-        public static final double kV = 0;//.1.335
+        public static final double kG = 0;
+        public static final double kA = 0;
         public static final double kP = 0;//40
         public static final double kI = 0;
         public static final double kD = 0;//.01
