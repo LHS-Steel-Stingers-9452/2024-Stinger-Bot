7bd67ee293a643f1c3d75bda9b08b495fe5629d5
kevin <kevinjaramillo247@gamil.com>
2024-04-04 12:05:37 -0700

    keep

--- Stinger-Bot/src/main/java/frc/robot/subsystems/launcher/Shooter.java
+++ Stinger-Bot/src/main/java/frc/robot/subsystems/launcher/Shooter.java
@@ -78,6 +78,7 @@ public class Shooter extends SubsystemBase {
     /* Apply configs */
     motorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
     topLauncher.getConfigurator().apply(motorConfig);
+    bottomLauncher.setControl(new Follower(topLaunchID, false));
 
     // optimize StatusSignal rates for the Talons
     topLauncher.getVelocity().setUpdateFrequency(50);
@@ -144,9 +145,6 @@ public class Shooter extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-
-    bottomLauncher.setControl(new Follower(topLaunchID, false));
-
     canShoot.setBoolean(areWheelsAtSpeed());
     shooterVeloc.setDouble(getShooterVelocity());
     shooterVeloc.setDouble(getShooterVelocityRPM());
