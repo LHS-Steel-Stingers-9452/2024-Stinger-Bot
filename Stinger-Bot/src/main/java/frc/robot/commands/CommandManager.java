52622bee10b61649e8714487529d03badb923ba1
kevin <kevinjaramillo247@gamil.com>
2024-04-12 19:38:12 -0700

    Updated estop, added resRest

--- Stinger-Bot/src/main/java/frc/robot/commands/CommandManager.java
+++ Stinger-Bot/src/main/java/frc/robot/commands/CommandManager.java
@@ -1,10 +1,16 @@
 package frc.robot.commands;
 
+import javax.swing.DebugGraphics;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.TransferConstants;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.Arm.PivotStates;
 import frc.robot.subsystems.drive.SwerveBase;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.transfer.Transfer;
@@ -29,11 +35,19 @@ public class CommandManager {
         return command;
     }
 
-    public static Command eStop(Intake intake, Transfer transfer){
+    public static Command stopIntaking(Intake intake, Transfer transfer){
         Command command = new ParallelCommandGroup(
             new InstantCommand(() -> transfer.stopTransfer(), transfer),
             new InstantCommand(() -> intake.stopIntake(), intake));
+        return command;
+    }
 
+    public static Command eStop(Intake intake, Transfer transfer, Arm arm, Shooter shooter){
+        Command command = new ParallelCommandGroup(
+            new InstantCommand(() -> transfer.stopTransfer(), transfer),
+            new InstantCommand(() -> intake.stopIntake(), intake),
+            new InstantCommand(() -> arm.requestState(PivotStates.DefaultState)),
+            new InstantCommand(() -> shooter.stopShooter()));
         return command;
     }
 
@@ -49,6 +63,14 @@ public class CommandManager {
     public static Command zeroGyro(SwerveBase swerveBase){
         Command command = 
             new InstantCommand(() -> swerveBase.zeroGyro());
+        return command;
+    }
+
+    public static Command redReset(SwerveBase swerveBase, double degrees){
+        Command command =
+            new InstantCommand(
+                ()-> swerveBase.setHeading(new Rotation2d(Units.degreesToRadians(degrees))));
+
         return command;
     }
 
