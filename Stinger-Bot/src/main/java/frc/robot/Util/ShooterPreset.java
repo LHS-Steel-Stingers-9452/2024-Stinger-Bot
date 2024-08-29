d70209dcb1fe273e8244d83243813d214cd2967c
Kevin Jaramillo <77587821+kevinjaramillo@users.noreply.github.com>
2024-08-29 16:56:04 -0700

    File ready

--- Stinger-Bot/src/main/java/frc/robot/Util/ShooterPreset.java
+++ Stinger-Bot/src/main/java/frc/robot/Util/ShooterPreset.java
@@ -1,5 +1,30 @@
 package frc.robot.Util;
 
 public class ShooterPreset {
+    private double armAngle;
+    private double shooterVeloc;
+
+    public ShooterPreset(double armAngle, double shooterVeloc){
+        this.armAngle = armAngle;
+        this.shooterVeloc = shooterVeloc;
+    }
+
+    //SECTION - getters
+    public double getArmAngle(){
+        return armAngle;
+    }
+
+    public double getShooterVeloc(){
+        return shooterVeloc;
+    }
+
+    //SECTION -  setters()
+    public void setArmAngle(double newAngle){
+        this.armAngle = newAngle;
+    }
+
+    public void setShooterVeloc(double newSpeed){
+        this.shooterVeloc = newSpeed;
+    }
     
 }
