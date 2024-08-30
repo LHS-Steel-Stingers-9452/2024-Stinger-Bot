be8172be68d1afa40a99da591ad0e52e844fe648
Kevin Jaramillo <77587821+kevinjaramillo@users.noreply.github.com>
2024-08-29 17:14:46 -0700

    Nvm HashMap is a no go

--- Stinger-Bot/src/main/java/frc/robot/Util/ShooterPreset.java
+++ Stinger-Bot/src/main/java/frc/robot/Util/ShooterPreset.java
@@ -1,15 +1,21 @@
 package frc.robot.Util;
 
 public class ShooterPreset {
+    private double distance;
     private double armAngle;
     private double shooterVeloc;
 
-    public ShooterPreset(double armAngle, double shooterVeloc){
+    public ShooterPreset(double distance, double armAngle, double shooterVeloc){
+        this.distance = distance;
         this.armAngle = armAngle;
         this.shooterVeloc = shooterVeloc;
     }
 
     //SECTION - getters
+    public double getDistance(){
+        return distance;
+    }
+    
     public double getArmAngle(){
         return armAngle;
     }
@@ -19,6 +25,10 @@ public class ShooterPreset {
     }
 
     //SECTION -  setters()
+    public void seDistance(double newDistance){
+        this.distance = newDistance;
+    }
+
     public void setArmAngle(double newAngle){
         this.armAngle = newAngle;
     }
