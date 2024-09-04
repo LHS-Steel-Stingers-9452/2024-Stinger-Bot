24193eb47fe15d0904faf4e1916df7f193f9ab16
Kevin Jaramillo <77587821+kevinjaramillo@users.noreply.github.com>
2024-09-04 15:41:10 -0700

    Layout notes

--- Stinger-Bot/src/main/java/frc/robot/commands/addVisionEst.java
+++ Stinger-Bot/src/main/java/frc/robot/commands/addVisionEst.java
@@ -1,6 +1,11 @@
 package frc.robot.commands;
 
 public class addVisionEst {
-    //aim command 
     //shoot command
+    /**
+     * This Command should first make sure that the robot is in shooting mode meaning that the robot is aligned with speaker
+     * we can utilize a simple public enum for this in  SwerveBase.java
+     * option two is to just make a method where we see if the robot rotation difference with speaker is 0 and if it returns
+     * true then proceed with setting arm 
+     */
 }
